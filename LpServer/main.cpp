bd6dbb1f3c0eada6cbaccf1e551edcb3d04293b2
Lueper <jkl410907@gmail.com>
2024-07-22 22:36:27 +0900

    Debug: 메모리 누수 체크

--- LpServer/main.cpp
+++ LpServer/main.cpp
@@ -1,8 +1,22 @@
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
 #include <iostream>
 
 #include "LpServer.h"
 
+#ifdef _DEBUG
+#define malloc(s) _malloc_dbg(s, _NORMAL_BLOCK, __FILE__, __LINE__)
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#endif
+
 int main() {
+
+#ifdef _DEBUG
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+    _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+#endif
 
     LpServer lpServer("127.0.0.1", 5000);
 

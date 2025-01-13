9b42539340c266ef71ae8d6f2c0c32bc43087324
Lueper <jkl410907@gmail.com>
2025-01-13 19:30:18 +0900

    Feature: LpPacketHandler Deserialize()

--- LpNetCore/LpPacketHandler.cpp
+++ LpNetCore/LpPacketHandler.cpp
@@ -21,24 +21,26 @@ T* LpPacketHandler::Deserialize(const char* _data) {
 	}
 
 	uint16_t offset = 0;
-	T* data = nullptr;
+	T* data = (T*)malloc(sizeof(T));
 	memset(data, 0, sizeof(T));
 
-	memcpy_s(data, sizeof(data), _data + offset, sizeof(data));
+	memcpy_s(data, sizeof(T), _data + offset, sizeof(T));
 
 	return data;
 }
 
 void LpPacketHandler::Process(const char* _data, uint32_t _size) {
-	if (_size >= sizeof(PacketHeader) + sizeof(PacketTail))
-
-	if (packetHeader->size < _size) {
+	if (_size < sizeof(PacketHeader)) {
 		return;
 	}
 
 	PacketHeader* packetHeader = Deserialize<PacketHeader>(_data);
 
 	if (packetHeader == nullptr) {
+		return;
+	}
+
+	if (packetHeader->size < _size) {
 		return;
 	}
 
@@ -50,12 +52,47 @@ void LpPacketHandler::Process(const char* _data, uint32_t _size) {
 	}
 
 	switch (packetHeader->type) {
+		case 100:
+			// ÀÏ´Ü 100 ¸¸ ½ÂÀÎ
+			break;
 		default:
-		break;
+			return;
 	}
 
-	
+	if (_size != sizeof(Packet)) {
+		return;
+	}
 
-	//std::cout << packet->header.type << "\n";
+	char* packetPayload = reinterpret_cast<char*>(Deserialize<char[128]>(_data + sizeof(PacketHeader)));
+
+	if (packetPayload == nullptr) {
+		return;
+	}
+
+	PacketTail* packetTail = Deserialize<PacketTail>(_data + sizeof(PacketHeader) + sizeof(char[128]));
+
+	if (packetTail == nullptr) {
+		return;
+	}
+
+	switch (packetTail->value) {
+		case 255:
+			// 255 ¸¸ ½ÂÀÎ
+			break;
+		default:
+			return;
+	}
+
+	//LpDecryptor.Decode(checkSum)
+
+	std::cout << "[Info]#LpPacketHandler : "
+		<< "[type:" << (uint8_t)packetHeader->type << "]"
+		<< "[size:" << (uint32_t)packetHeader->size << "]"
+		<< "[payload:" << packetPayload << "]"
+		<< "\n";
+
+	delete packetHeader;
+	delete packetPayload;
+	delete packetTail;
 }
 }

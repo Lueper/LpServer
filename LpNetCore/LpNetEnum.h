6b18e90ea1266af43dde5b60b05d7597b5fd96ce
Lueper <jkl410907@gmail.com>
2025-02-24 18:59:16 +0900

    Feature: Add Enum class - IOType, NetEvent

--- LpNetCore/LpNetEnum.h
+++ LpNetCore/LpNetEnum.h
@@ -1,9 +1,24 @@
 #pragma once
 
 namespace lpnet {
-enum SessionState {
+enum class SessionState : uint8_t {
 	Closed = 0,
 	Connecting,
 	Connected
+};
+
+enum class IOType : uint8_t {
+	None = 0,
+	Read,
+	Write
+};
+
+enum class NetEvent : uint8_t {
+	None = 0,
+	Accept,
+	Connect,
+	Read,
+	Write,
+	Wait
 };
 }

287dd2affe975c88e9a019b87488605ace596bcd
Lueper <jkl410907@gmail.com>
2025-03-05 15:56:00 +0900

    Etc: LpLogger 변수명 수정

--- LpUtility/LpLogger.cpp
+++ LpUtility/LpLogger.cpp
@@ -4,22 +4,18 @@
 namespace lpnet {
 std::mutex LpLogger::m_mutex;
 
-void LpLogger::LOG(ELogType _logType, const wchar_t* msg) {
-	std::wstring ws(msg);
+void LpLogger::LOG(ELogType _logType, const wchar_t* _msg) {
+	std::wstring ws(_msg);
 	LOG(_logType, std::string(ws.begin(), ws.end()));
 }
 
-void LpLogger::LOG(ELogType _logType, const std::string& msg) {
-	std::lock_guard<std::mutex> lock(m_mutex);
-
+void LpLogger::LOG(ELogType _logType, const std::string& _msg) {
 	std::ostringstream os;
 	struct _timeb	  _time;
 	tm				   t;
 
 	_ftime64_s(&_time);
 	localtime_s(&t, &(_time.time));
-	
-	setColor(_logType);
 
 	// [yyyy-mm-dd hh:mm:ss.ms]
 	os << t.tm_year + 1900 << ("-");
@@ -58,14 +54,21 @@ void LpLogger::LOG(ELogType _logType, const std::string& msg) {
 	std::thread::id threadID = std::this_thread::get_id();
 	os << threadID << ("] ");
 
-	os << msg << std::endl;
+	os << _msg << "\n";
 
-	std::cout << os.str();
-
-	resetColor();
+	Print(_logType, os.str());
 }
 
-void LpLogger::setColor(ELogType _logType) {
+void LpLogger::Print(ELogType _logType, const std::string& _os) {
+	std::lock_guard<std::mutex> lock(m_mutex);
+
+	SetColor(_logType);
+	std::cout << _os;
+	//WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), _os.c_str(), static_cast<DWORD>(_os.size()), NULL, NULL);
+	ResetColor();
+}
+
+void LpLogger::SetColor(ELogType _logType) {
 	switch (_logType) {
 	case ELogType::debug:	// cyan
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
@@ -86,7 +89,7 @@ void LpLogger::setColor(ELogType _logType) {
 	}
 }
 
-void LpLogger::resetColor() {
+void LpLogger::ResetColor() {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 }
 }
--- LpUtility/LpLogger.h
+++ LpUtility/LpLogger.h
@@ -30,20 +30,24 @@ const std::string LOG_DESC[]{
 };
 
 namespace lpnet {
-#define LOG_DEBUG(msg)	LOG(ELogType::debug, msg)
-#define LOG_INFO(msg)	LOG(ELogType::info, msg)
-#define LOG_WARN(msg)	LOG(ELogType::warn, msg)
-#define LOG_ERROR(msg)	LOG(ELogType::error, msg)
-#define LOG_FATAL(msg)	LOG(ELogType::fatal, msg)
+#define LOG_DEBUG(_msg)	LOG(ELogType::debug, _msg)
+#define LOG_INFO(_msg)	LOG(ELogType::info, _msg)
+#define LOG_WARN(_msg)	LOG(ELogType::warn, _msg)
+#define LOG_ERROR(_msg)	LOG(ELogType::error, _msg)
+#define LOG_FATAL(_msg)	LOG(ELogType::fatal, _msg)
 
 class LpLogger : LpSingleton<LpLogger> {
 public:
-	static void LOG(ELogType _logType, const wchar_t* msg);
-	static void LOG(ELogType _logType, const std::string& msg);
+	LpLogger();
+
+	static void LOG(ELogType _logType, const wchar_t* _msg);
+	static void LOG(ELogType _logType, const std::string& _msg);
 
 	static std::mutex m_mutex;
 private:
-	static void setColor(ELogType _logType);
-	static void resetColor();
+	static void Print(ELogType _logType, const std::string& _os);
+
+	static void SetColor(ELogType _logType);
+	static void ResetColor();
 };
 }
